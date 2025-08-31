e9879a3f537476a3a94ac71f093287213b75b20d
PARK-SANGGEUN <p3412@nate.com>
2025-08-31 09:35:10 +0900

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Admission Search</title>
+      </Head>
+      <main>
+        <h1>Admission Search</h1>
+        <p>입시결과 검색 웹앱</p>
+      </main>
+    </div>
+  )
+}
